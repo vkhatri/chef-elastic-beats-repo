544670196473d7e82d7564d9f377aba501023947
Virender Khatri <vir.khatri@gmail.com>
2018-04-20 19:01:38 +0530

    fixed apt cookbook resource dependency version

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ source_url 'https://github.com/vkhatri/chef-elastic-beats-repo' if respond_to?(:
 issues_url 'https://github.com/vkhatri/chef-elastic-beats-repo/issues' if respond_to?(:issues_url)
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-depends 'apt'
+depends 'apt', '~> 6.1.4'
 depends 'yum', '>= 5.0.1'
 
 %w[debian ubuntu centos amazon redhat fedora].each do |os|
