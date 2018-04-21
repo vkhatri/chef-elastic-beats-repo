59eb907a88d63b6ffc74a9d4090f963243d5966c
Virender Khatri <vir.khatri@gmail.com>
2018-04-21 15:45:04 +0530

    release cookbook version v1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,19 @@
 elastic_beats_repo CHANGELOG
 ==================
 
+1.2.0
+-----
+
+- Virender Khatri - corrected apt cookbook version dependency
+
+
 1.1.0
 -----
 
 - Virender Khatri - added apt cookbook version dependency
 
 - Virender Khatri - updated kitchen inspec tests
+
 
 1.0.0
 -----
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vir.khatri@gmail.com'
 license 'Apache-2.0'
 description 'Installs/Configures Elastic Beats APT/Yum Repository'
 long_description 'Installs/Configures Elastic Beats APT/Yum Repository'
-version '1.1.0'
+version '1.2.0'
 source_url 'https://github.com/vkhatri/chef-elastic-beats-repo' if respond_to?(:source_url)
 issues_url 'https://github.com/vkhatri/chef-elastic-beats-repo/issues' if respond_to?(:issues_url)
 chef_version '>= 12.1' if respond_to?(:chef_version)
